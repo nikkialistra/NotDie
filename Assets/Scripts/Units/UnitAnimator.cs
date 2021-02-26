4604874c35f2c51b313417fee0ca15313291449a
nocrater <nocrater@gmail.com>
2021-02-27 03:58:33 +0500

    Add enemy base states

--- Assets/Scripts/Units/UnitAnimator.cs
+++ Assets/Scripts/Units/UnitAnimator.cs
@@ -1,46 +1,49 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(Rigidbody2D))]
-public class UnitAnimator : MonoBehaviour
+namespace Units
 {
-    [SerializeField] private float _idleSpeed;
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class UnitAnimator : MonoBehaviour
+    {
+        [SerializeField] private float _idleSpeed;
     
     
-    private Animator _animator;
-    private Rigidbody2D _rigidbody;
+        private Animator _animator;
+        private Rigidbody2D _rigidbody;
 
-    private readonly int _isMoving = Animator.StringToHash("isMoving");
+        private readonly int _isMoving = Animator.StringToHash("isMoving");
 
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
 
-    private void Update()
-    {
-        SetMovingAnimation();
-        Flip();
-    }
+        private void Update()
+        {
+            SetMovingAnimation();
+            Flip();
+        }
 
-    private void SetMovingAnimation()
-    {
-        _animator.SetBool(_isMoving, _rigidbody.velocity.magnitude > _idleSpeed);
-    }
+        private void SetMovingAnimation()
+        {
+            _animator.SetBool(_isMoving, _rigidbody.velocity.magnitude > _idleSpeed);
+        }
     
-    private void Flip()
-    {
-        if (_rigidbody.velocity.x == 0)
-            return;
+        private void Flip()
+        {
+            if (_rigidbody.velocity.x == 0)
+                return;
         
-        var scale = transform.localScale;
+            var scale = transform.localScale;
 
-        if (_rigidbody.velocity.x > 0)
-            scale.x = 1;
-        else
-            scale.x = -1;
+            if (_rigidbody.velocity.x > 0)
+                scale.x = 1;
+            else
+                scale.x = -1;
         
-        transform.localScale = scale;
+            transform.localScale = scale;
+        }
     }
 }
