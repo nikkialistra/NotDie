5cc1ced0fd6153a9cac91452b45500ee5a62f7b0
nocrater <nocrater@gmail.com>
2021-03-08 00:06:17 +0500

    Add base wave spawning

--- Assets/Scripts/Entities/Wave/WaveFacade.cs
+++ Assets/Scripts/Entities/Wave/WaveFacade.cs
@@ -1,51 +1,51 @@
 ﻿using System;
+using Entities.Player;
 using UnityEngine;
 using Zenject;
 
 namespace Entities.Wave
 {
     [RequireComponent(typeof(WaveMover))]
-    public class WaveFacade : MonoBehaviour, IPoolable<Vector3, Vector2, Data.Wave, IMemoryPool>, IDisposable
+    [RequireComponent(typeof(Animator))]
+    public class WaveFacade : MonoBehaviour, IPoolable<Vector3, Vector2, IMemoryPool>, IDisposable
     {
         private IMemoryPool _pool;
         
         private WaveMover _waveMover;
-        
-        private float _timeToDestroy;
+
+        private Animator _animator;
+
         private int _damageValue;
 
         public int DamageValue => _damageValue;
 
-        private void Awake() => _waveMover = GetComponent<WaveMover>();
+        private void Awake()
+        {
+            _waveMover = GetComponent<WaveMover>();
+            _animator = GetComponent<Animator>();
+        }
 
         private void Update()
         {
-            if (_timeToDestroy <= 0)
-                return;
-            
-            _timeToDestroy -= Time.deltaTime;
-            if (_timeToDestroy <= 0)
+            if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
                 Dispose();
         }
 
-        public void OnSpawned(Vector3 position, Vector2 direction, Data.Wave wave, IMemoryPool pool)
+        public void OnSpawned(Vector3 position, Vector2 direction, IMemoryPool pool)
         {
             _pool = pool;
             
-            transform.position = position;
-
-            _waveMover.SetVelocity(wave.Velocity);
+            _waveMover.SetPosition(position);
+            _waveMover.SetVelocity(100);
             _waveMover.SetDirection(direction);
             
-            _timeToDestroy = wave.TimeToDestroy;
-            _damageValue = wave.DamageValue;
         }
 
         public void Dispose() => _pool.Despawn(this);
         
         public void OnDespawned() => _pool = null;
-        
-        public class Factory : PlaceholderFactory<Vector3, Vector2, Data.Wave, WaveFacade>
+
+        public class Factory : PlaceholderFactory<Vector3, Vector2, WaveFacade>
         {
         }
     }
